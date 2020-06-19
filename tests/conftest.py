40f0b5d503aa3b8aa79674cb9f40c386ebb0d0ad
Arne <arne-@hotmail.de>
2020-06-19 10:20:21 +0200

    Added curio-async marker for asynchronous test routines

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+import inspect
+import curio
+
+@pytest.mark.tryfirst
+def pytest_pycollect_makeitem(collector, name, obj):
+    """
+    From https://docs.pytest.org/en/latest/reference.html
+    return custom item/collector for a python object in a module, or None
+    Stops at first non-None result, see firstresult: stop at first non-None result
+    """
+    if collector.funcnamefilter(name) and inspect.iscoroutinefunction(obj):
+        item = pytest.Function(name, parent=collector)
+        if 'curio' in item.keywords:
+            return list(collector._genfunctions(name, obj))
+
+
+@pytest.mark.tryfirst
+def pytest_pyfunc_call(pyfuncitem):
+    """
+    From https://docs.pytest.org/en/latest/reference.html
+    call underlying test function.
+    Stops at first non-None result, see firstresult: stop at first non-None result
+    """
+    if 'curio' in pyfuncitem.keywords:
+        kernel = curio.Kernel()
+        funcargs = pyfuncitem.funcargs
+        testargs = {arg: funcargs[arg] for arg in pyfuncitem._fixtureinfo.argnames}
+        fut = pyfuncitem.obj(**testargs)
+        kernel.run(fut)
+        return True
