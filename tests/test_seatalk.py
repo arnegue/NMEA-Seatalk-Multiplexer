054004a0d011a9c5a1f6be3b688ae2266f794d5c
Arne <arne-@hotmail.de>
2020-06-19 13:07:52 +0200

    Removal of unused prints in test

--- tests/test_seatalk.py
+++ tests/test_seatalk.py
@@ -1,5 +1,4 @@
 import pytest
-import helper
 
 import device_io
 import seatalk
@@ -63,14 +62,12 @@ async def test_not_recognized():
 
 @pytest.mark.parametrize(("seatalk_datagram", "expected_bytes"), (
         (seatalk.DepthDatagram(depth_m=22.3),      bytes([0x00, 0x02, 0x00, 0xDB, 0x02])),               # 22.3m  -> 73.16f -> 731.6 -> 731  -> 0x02DB -> 0xDB02
-        (seatalk.SpeedDatagram(speed_knots=8.31),  bytes([0x20, 0x01, 0x53, 0x00])),                     # 8.3nm  ->           83.1  ->  83  -> 0x0053 -> 0x5300
-        (seatalk.SpeedDatagram2(speed_knots=5.19), bytes([0x26, 0x04, 0x07, 0x02, 0x00, 0x00, 0x00])),   # 5.19nm ->           5190  ->      -> 0x0207 -> 0x0702
+        (seatalk.SpeedDatagram(speed_knots=8.31),  bytes([0x20, 0x01, 0x53, 0x00])),                     # 8.3nm  ->        -> 83.1  ->  83  -> 0x0053 -> 0x5300
+        (seatalk.SpeedDatagram2(speed_knots=5.19), bytes([0x26, 0x04, 0x07, 0x02, 0x00, 0x00, 0x00])),   # 5.19nm ->        -> 5190  ->      -> 0x0207 -> 0x0702
         (seatalk.WaterTemperatureDatagram(17.2),   bytes([0x23, 0x01, 0x11, 0x3E])),                     # 17.2C  -> 17     ->       ->      -> 0x0011 -> 0x1100
         (seatalk.WaterTemperatureDatagram2(19.2),  bytes([0x27, 0x01, 0xA8, 0x04])),                     # 19.2   ->        -> 119.2 -> 1192 -> 0x04A8 -> 0xA804
         (seatalk.SetLampIntensityDatagram(3),      bytes([0x30, 0x00, 0x0C]))
 ))
 def test_check_datagram_to_seatalk(seatalk_datagram, expected_bytes):
     actual_datagram = seatalk_datagram.get_seatalk_datagram()
-    expected = helper.bytes_to_str(expected_bytes)
-    actual = helper.bytes_to_str(actual_datagram)
     assert expected_bytes == actual_datagram
