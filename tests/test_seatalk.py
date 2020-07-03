86bae51f15c9b6c866a8703d7ffdfbaa8b05d046
Arne <arne-@hotmail.de>
2020-07-03 14:12:03 +0200

    organised import of seatalkdatagram

--- tests/test_seatalk.py
+++ tests/test_seatalk.py
@@ -2,8 +2,9 @@ import pytest
 import datetime
 
 import device_io
-from seatalk import seatalk, seatalk_datagram
-from helper import  bytes_to_str
+from seatalk import seatalk
+from seatalk.seatalk_datagram import *
+from helper import bytes_to_str
 
 
 class TestValueReceiver(device_io.IO):
@@ -22,36 +23,36 @@ class TestValueReceiver(device_io.IO):
 
 def get_parameters():
     return ("seatalk_datagram", "byte_representation"), (
-       (seatalk_datagram.DepthDatagram(depth_m=22.3),      bytes([0x00, 0x02, 0x00, 0xDB, 0x02])),
-       (seatalk_datagram.EquipmentIDDatagram1(seatalk_datagram.EquipmentIDDatagram1.Equipments.ST60_Tridata), bytes([0x01, 0x05, 0x04, 0xBA, 0x20, 0x28, 0x01, 0x00])),
-       (seatalk_datagram.ApparentWindAngleDatagram(256.5), bytes([0x10, 0x01, 0x01, 0x02])),
-       (seatalk_datagram.ApparentWindSpeedDatagram(18.3),  bytes([0x11, 0x01, 0x12, 0x03])),
-       (seatalk_datagram.SpeedDatagram(speed_knots=8.31),  bytes([0x20, 0x01, 0x53, 0x00])),
-       (seatalk_datagram.TripMileage(6784.12),             bytes([0x21, 0x02, 0x0C, 0x5A, 0x0A])),
-       (seatalk_datagram.TotalMileage(6553),               bytes([0x22, 0x02, 0xFA, 0xFF, 0x00])),
-       (seatalk_datagram.WaterTemperatureDatagram(17.2),   bytes([0x23, 0x01, 0x11, 0x3E])),
-       (seatalk_datagram.SpeedDatagram2(speed_knots=5.19), bytes([0x26, 0x04, 0x07, 0x02, 0x00, 0x00, 0x00])),
-       (seatalk_datagram.WaterTemperatureDatagram2(19.2),  bytes([0x27, 0x01, 0xA8, 0x04])),
-       (seatalk_datagram.SetLampIntensity1(3),             bytes([0x30, 0x00, 0x0C])),
-       (seatalk_datagram.CodeLockData(x=15, y=248, z=1),   bytes([0x38, 0xF1, 0xF8, 0x01])),
-       (seatalk_datagram.CancelMOB(),                      bytes([0x36, 0x00, 0x01])),
-       (seatalk_datagram.Date(date=datetime.date(year=2019, month=10, day=31)), bytes([0x56, 0xA1, 0x1F, 0x13])),
-       (seatalk_datagram.SpeedOverGround(26.9),            bytes([0x52, 0x01, 0x0D, 0x01])),
-       (seatalk_datagram.KeyStroke1(key=seatalk_datagram.KeyStroke1.Key.M1M10PortTack, increment_decrement=1), bytes([0x55, 0x11, 0x21, 0xDE])),
-       (seatalk_datagram.SatInfo(0x1, 0x94),               bytes([0x57, 0x10, 0x94])),
-       (seatalk_datagram.CountDownTimer(hours=9, minutes=59, seconds=59, mode=seatalk_datagram.CountDownTimer.CounterMode.CountDown), bytes([0x59, 0x22, 0x3B, 0x3B, 0x49])),
-       (seatalk_datagram.E80Initialization(),              bytes([0x61, 0x03, 0x03, 0x00, 0x00, 0x00])),
-       (seatalk_datagram.SelectFathom(),                   bytes([0x65, 0x00, 0x02])),
-       (seatalk_datagram.WindAlarm(seatalk_datagram.WindAlarm.Alarm.AngleLow, seatalk_datagram.WindAlarm.Alarm.SpeedHigh), bytes([0x66, 0x00, 0x81])),
-       (seatalk_datagram.AlarmAcknowledgement(seatalk_datagram.AlarmAcknowledgement.AcknowledgementAlarms.DeepWaterAlarm), bytes([0x68, 0x21, 0x01, 0x00])),
-       (seatalk_datagram.EquipmentIDDatagram2(seatalk_datagram.EquipmentIDDatagram2.Equipments.ST60_Log), bytes([0x6C, 0x05, 0x05, 0x70, 0x99, 0x10, 0x28, 0x2D])),
-       (seatalk_datagram.ManOverBoard(),                   bytes([0x6E, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])),
-       (seatalk_datagram.SetLampIntensity2(1),             bytes([0x80, 0x00, 0x04])),
-       (seatalk_datagram.KeyStroke2(key=seatalk_datagram.KeyStroke2.Key.StandbyAutoGT1S), bytes([0x86, 0x01, 0x63, 0x9C])),
-       (seatalk_datagram.SetResponseLevel(response_level=seatalk_datagram.SetResponseLevel.Deadband.Minimum), bytes([0x87, 0x00, 0x02])),
-       (seatalk_datagram.DeviceIdentification1(seatalk_datagram.DeviceIdentification1.DeviceID.ST600R), bytes([0x90, 0x00, 0x02])),
-       (seatalk_datagram.SetRudderGain(3),                 bytes([0x91, 0x00, 0x03])),
-       (seatalk_datagram.EnterAPSetup(),                   bytes([0x93, 0x00, 0x00])),
+       (DepthDatagram(depth_m=22.3),                                                                bytes([0x00, 0x02, 0x00, 0xDB, 0x02])),
+       (EquipmentIDDatagram1(EquipmentIDDatagram1.Equipments.ST60_Tridata),                         bytes([0x01, 0x05, 0x04, 0xBA, 0x20, 0x28, 0x01, 0x00])),
+       (ApparentWindAngleDatagram(256.5),                                                           bytes([0x10, 0x01, 0x01, 0x02])),
+       (ApparentWindSpeedDatagram(18.3),                                                            bytes([0x11, 0x01, 0x12, 0x03])),
+       (SpeedDatagram(speed_knots=8.31),                                                            bytes([0x20, 0x01, 0x53, 0x00])),
+       (TripMileage(6784.12),                                                                       bytes([0x21, 0x02, 0x0C, 0x5A, 0x0A])),
+       (TotalMileage(6553),                                                                         bytes([0x22, 0x02, 0xFA, 0xFF, 0x00])),
+       (WaterTemperatureDatagram(17.2),                                                             bytes([0x23, 0x01, 0x11, 0x3E])),
+       (SpeedDatagram2(speed_knots=5.19),                                                           bytes([0x26, 0x04, 0x07, 0x02, 0x00, 0x00, 0x00])),
+       (WaterTemperatureDatagram2(19.2),                                                            bytes([0x27, 0x01, 0xA8, 0x04])),
+       (SetLampIntensity1(3),                                                                       bytes([0x30, 0x00, 0x0C])),
+       (CodeLockData(x=15, y=248, z=1),                                                             bytes([0x38, 0xF1, 0xF8, 0x01])),
+       (CancelMOB(),                                                                                bytes([0x36, 0x00, 0x01])),
+       (Date(date=datetime.date(year=2019, month=10, day=31)),                                      bytes([0x56, 0xA1, 0x1F, 0x13])),
+       (SpeedOverGround(26.9),                                                                      bytes([0x52, 0x01, 0x0D, 0x01])),
+       (KeyStroke1(key=KeyStroke1.Key.M1M10PortTack, increment_decrement=1),                        bytes([0x55, 0x11, 0x21, 0xDE])),
+       (SatInfo(0x1, 0x94),                                                                         bytes([0x57, 0x10, 0x94])),
+       (CountDownTimer(hours=9, minutes=59, seconds=59, mode=CountDownTimer.CounterMode.CountDown), bytes([0x59, 0x22, 0x3B, 0x3B, 0x49])),
+       (E80Initialization(),                                                                        bytes([0x61, 0x03, 0x03, 0x00, 0x00, 0x00])),
+       (SelectFathom(),                                                                             bytes([0x65, 0x00, 0x02])),
+       (WindAlarm(WindAlarm.Alarm.AngleLow, WindAlarm.Alarm.SpeedHigh),                             bytes([0x66, 0x00, 0x81])),
+       (AlarmAcknowledgement(AlarmAcknowledgement.AcknowledgementAlarms.DeepWaterAlarm),            bytes([0x68, 0x21, 0x01, 0x00])),
+       (EquipmentIDDatagram2(EquipmentIDDatagram2.Equipments.ST60_Log),                             bytes([0x6C, 0x05, 0x05, 0x70, 0x99, 0x10, 0x28, 0x2D])),
+       (ManOverBoard(),                                                                             bytes([0x6E, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])),
+       (SetLampIntensity2(1),                                                                       bytes([0x80, 0x00, 0x04])),
+       (KeyStroke2(key=KeyStroke2.Key.StandbyAutoGT1S),                                             bytes([0x86, 0x01, 0x63, 0x9C])),
+       (SetResponseLevel(response_level=SetResponseLevel.Deadband.Minimum),                         bytes([0x87, 0x00, 0x02])),
+       (DeviceIdentification1(DeviceIdentification1.DeviceID.ST600R),                               bytes([0x90, 0x00, 0x02])),
+       (SetRudderGain(3),                                                                           bytes([0x91, 0x00, 0x03])),
+       (EnterAPSetup(),                                                                             bytes([0x93, 0x00, 0x00])),
     )
 
 
@@ -70,7 +71,7 @@ async def test_correct_recognition(seatalk_datagram, byte_representation):
 async def test_not_enough_data():
     original = bytes([0x00, 0x01, 0x00, 0x00])
     seatalk_device = seatalk.SeatalkDevice("TestDevice", io_device=TestValueReceiver(original))
-    with pytest.raises(seatalk_datagram.NotEnoughData):
+    with pytest.raises(NotEnoughData):
         await seatalk_device.receive_data_gram()
 
 
@@ -78,7 +79,7 @@ async def test_not_enough_data():
 async def test_too_much_data():
     original = bytes([0x00, 0x03, 0x00, 0x00, 0x00, 0x00])
     seatalk_device = seatalk.SeatalkDevice("TestDevice", io_device=TestValueReceiver(original))
-    with pytest.raises(seatalk_datagram.TooMuchData):
+    with pytest.raises(TooMuchData):
         await seatalk_device.receive_data_gram()
 
 
@@ -98,11 +99,11 @@ def test_check_datagram_to_seatalk(seatalk_datagram, byte_representation):
 
 
 @pytest.mark.parametrize("seatalk_datagram_instance", (
-    seatalk_datagram.EquipmentIDDatagram1(9),
-    seatalk_datagram.SetLampIntensity1(9)
+    EquipmentIDDatagram1(9),
+    SetLampIntensity1(9)
 ))
 def test_two_way_maps_validations(seatalk_datagram_instance):
-    with pytest.raises(seatalk_datagram.DataValidationException):
+    with pytest.raises(DataValidationException):
         seatalk_datagram_instance.get_seatalk_datagram()
 
 
@@ -122,9 +123,9 @@ async def test_raw_seatalk():
 
 def get_device_identification_2_parameters():
     return ("seatalk_datagram", "byte_representation"), (
-        (seatalk_datagram.DeviceIdentification2.BroadCast(),     bytes([0xA4, 0x02, 0x00, 0x00, 0x00])),
-        (seatalk_datagram.DeviceIdentification2.Termination(),   bytes([0xA4, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])),
-        (seatalk_datagram.DeviceIdentification2.Answer(seatalk_datagram.DeviceIdentification2.Answer.DeviceID.RudderAngleIndicator, 0x34, 0x89), bytes([0xA4, 0x12, 0x0F, 0x34, 0x89]))
+        (DeviceIdentification2.BroadCast(),     bytes([0xA4, 0x02, 0x00, 0x00, 0x00])),
+        (DeviceIdentification2.Termination(),   bytes([0xA4, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])),
+        (DeviceIdentification2.Answer(DeviceIdentification2.Answer.DeviceID.RudderAngleIndicator, 0x34, 0x89), bytes([0xA4, 0x12, 0x0F, 0x34, 0x89]))
     )
 
 
