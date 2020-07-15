86637eb2acde861ea690119949a2c7765d4729d3
Arne <arne-@hotmail.de>
2020-07-15 15:14:32 +0200

    Added seatalk-id 0x99 - Magnetic Variation

--- seatalk/seatalk_datagram.py
+++ seatalk/seatalk_datagram.py
@@ -1205,6 +1205,27 @@ class EnterAPSetup(_ZeroContentClass):
         _ZeroContentClass.__init__(self, seatalk_id=0x93, data_length=0)
 
 
+class CompassVariation(SeatalkDatagram):
+    """
+    99  00  XX       Compass variation sent by ST40 compass instrument
+                     or ST1000, ST2000, ST4000+, E-80 every 10 seconds
+                     but only if the variation is set on the instrument
+                     Positive XX values: Variation West, Negative XX values: Variation East
+                     Examples (XX => variation): 00 => 0, 01 => -1 west, 02 => -2 west ...
+                                                 FF => +1 east, FE => +2 east ...
+                     Corresponding NMEA sentences: RMC, HDG
+    """
+    def __init__(self, variation=None):
+        SeatalkDatagram.__init__(self, seatalk_id=0x99, data_length=0)
+        self.variation = variation
+
+    def process_datagram(self, first_half_byte, data):
+        self.variation = int.from_bytes(bytes([data[0]]), byteorder="big", signed=True)  # TODO unsure if variation *-1
+
+    def get_seatalk_datagram(self):
+        return self.seatalk_id + bytearray([self.data_length]) + int.to_bytes(self.variation, byteorder="big", signed=True, length=1)
+
+
 class DeviceIdentification2(SeatalkDatagram):
     """
     Special class, which basically holds 3 other classes (depending on length and first half byte)
