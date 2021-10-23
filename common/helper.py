01f37163a67fde893720b108c9d7d4000391129f
Arne <arne-@hotmail.de>
2021-10-23 15:05:52 +0200

    Moved Singleton-Class to helper

--- common/helper.py
+++ common/helper.py
@@ -1,5 +1,16 @@
 import enum
-import curio
+
+
+class Singleton(type):
+    """
+    Copied from: https://stackoverflow.com/questions/6760685/
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 
 def byte_to_str(byte):
--- logger.py
+++ logger.py
@@ -2,17 +2,7 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
-
-class Singleton(type):
-    """
-    Copied from: https://stackoverflow.com/questions/6760685/
-    """
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+from common.helper import Singleton
 
 
 class Logger(object):
