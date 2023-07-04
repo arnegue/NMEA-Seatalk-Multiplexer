119bfc63cf840a73a93a2dbb6c1376467936dd98
Arne G <arne-@hotmail.de>
2023-07-04 08:43:22 +0200

    Added windows- and test-dependencies

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 setup(
     name='nmea_seatalk_multiplexer',
     version='1.0',
-    url='',
+    url='https://github.com/arnegue/NMEA-Seatalk-Multiplexer/',
     license='',
     author='Frosty',
     author_email='',
-    description='', install_requires=['curio>=1.0', 'pyserial']
+    description='Multiplatform NMEA and Seatalk parsing and multiplexing library written in pure Python',
+    install_requires=['curio>=1.0',
+                      'pyserial',
+                      'pywin32 >= 1.0 ; platform_system=="Windows"'],
+    extras_require={
+        'tests': ['pytest']
+    }
 )
