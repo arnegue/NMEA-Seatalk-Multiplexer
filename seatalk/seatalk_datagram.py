3b30b4ca1fe47fba8cf51be4b168e33796fd2202
Arne <arne-@hotmail.de>
2024-03-11 16:33:24 +0100

    Fixed MOB (seems like it's not only 0-bytes)

--- seatalk/seatalk_datagram.py
+++ seatalk/seatalk_datagram.py
@@ -1181,16 +1181,22 @@ class EquipmentIDDatagram2(_TwoWayDictDatagram):
         _TwoWayDictDatagram.__init__(self, map=equipment_map, set_key=equipment_id)
 
 
-class ManOverBoard(_ZeroContentClass):
+class ManOverBoard(SeatalkDatagram):
     """
+    According to Thomas Knauf:
     6E  07  00  00 00 00 00 00 00 00 MOB (Man Over Board), (ST80), preceded
                  by a Waypoint 999 command: 82 A5 40 BF 92 6D 24 DB
+    I noticed on Raymarine RN300 (not sure about the byte meaning though):
+    6E  47  0F  E7 59 00 00 0F A7 70
     """
     seatalk_id = 0x6E
     data_length = 7
 
-    def __init__(self):
-        _ZeroContentClass.__init__(self)
+    def process_datagram(self, first_half_byte, data):
+        pass  # Nothing to do here
+
+    def get_seatalk_datagram(self):
+        return bytearray([self.seatalk_id, self.data_length, 0, 0, 0, 0, 0, 0, 0, 0])
 
 
 class SetLampIntensity2(_SetLampIntensityDatagram):
