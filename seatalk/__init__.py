c563e5818949dd472ec29bee5a3ceda94e7c98ef
Arne <arne-@hotmail.de>
2024-03-17 13:41:06 +0100

    #1 Moved CancelMOB to own file

--- seatalk/__init__.py
+++ seatalk/__init__.py
@@ -1,3 +1,4 @@
 from seatalk.seatalk import SeatalkDevice
 
 from seatalk.s00_depth_datagram import DepthDatagram
+from seatalk.s36_cancel_mob import CancelMOB
--- seatalk/seatalk_datagram.py
+++ seatalk/seatalk_datagram.py
@@ -410,26 +410,6 @@ class SetLampIntensity1(_SetLampIntensityDatagram):
     def __init__(self, intensity=0):
         _SetLampIntensityDatagram.__init__(self, intensity=intensity)
 
-
-class CancelMOB(SeatalkDatagram):
-    """
-    36  00  01      Cancel MOB (Man Over Board) condition
-    """
-    seatalk_id = 0x36
-    data_length = 0
-    
-    def __init__(self, *args, **kwargs):
-        SeatalkDatagram.__init__(self)
-        self._expected_byte = bytearray([0x01])
-
-    def process_datagram(self, first_half_byte, data):
-        if data != self._expected_byte:
-            raise DataValidationException(f"{type(self).__name__}: Expected {self._expected_byte}, got {data} instead.")
-
-    def get_seatalk_datagram(self):
-        return bytearray([self.seatalk_id, self.data_length]) + self._expected_byte
-
-
 class CodeLockData(SeatalkDatagram):
     """
     38  X1  YY  yy  CodeLock data
