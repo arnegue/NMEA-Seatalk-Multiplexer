841a8d1ee0483927e157e69c28250f1dc0b0cd9d
Arne <arne-@hotmail.de>
2024-03-17 13:42:21 +0100

    #1 Moved CodeLockData to own file

--- seatalk/__init__.py
+++ seatalk/__init__.py
@@ -2,3 +2,4 @@ from seatalk.seatalk import SeatalkDevice
 
 from seatalk.s00_depth_datagram import DepthDatagram
 from seatalk.s36_cancel_mob import CancelMOB
+from seatalk.s38_code_lock_data import CodeLockData
